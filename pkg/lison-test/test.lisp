e5cbd4549536d4d0c20ba5ba85b29ec925d29103
Keyboard Slayer <dalcjor@libmail.eu>
2022-02-03 21:42:00 +0100

    remove quotes in example

--- pkg/lison-test/test.lisp
+++ pkg/lison-test/test.lisp
@@ -1,11 +1,11 @@
 '(
     (entries (
-        '(
+        (
             (name  "Navy")
             (kernel  "/boot/kernel.elf")
         )
 
-        '(
+        (
             (name "Brutal")
             (kernel "/boot/brutal.elf")
         )
