65b6b8539ae6d50cfe8913f16ad9ec12f6f77c50
Keyboard Slayer <dalcjor@libmail.eu>
2022-04-02 21:09:59 +0200

    [FIX] stack was not mapped

--- kernel/task.c
+++ kernel/task.c
@@ -1,5 +1,8 @@
 #include "task.h"
 #include "arch.h"
+#include "brutal/result.h"
+#include "hw/x86_64/const.h"
+#include "hw/x86_64/vmm.h"
 
 #include <brutal/str.h>
 #include <stdlib.h>
@@ -9,10 +12,7 @@ Task *create_task(Str name, PmlOption space)
     Task *self = (Task *) calloc(sizeof(Task), 1);
     self->state = TASK_RUNNING;
     self->name = name;
-    self->stack = (Range) {
-        .base = (uintptr_t) calloc(STACK_SIZE, 1),
-        .length = STACK_SIZE
-    };
+    self->stack = UNWRAP_OR_PANIC(pmm_alloc(STACK_SIZE), "Out of memory");
 
     self->sp = self->stack.base + self->stack.length;
 
@@ -24,6 +24,8 @@ Task *create_task(Str name, PmlOption space)
     {
         self->space = UNWRAP_OR_PANIC(vmm_create_space(), "Couldn't create an address space");
     }
+
+    vmm_map_range(self->space, self->stack, self->stack, true);
 
     return self;
 }
